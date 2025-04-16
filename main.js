97d5d8b2f4bee383d10fddd8b8ce87c673b61971
Corina-Lago <corinalago7@gmail.com>
2025-04-16 17:00:27 -0300

    error data

--- main.js
+++ main.js
@@ -6,15 +6,48 @@ const mas = document.getElementById("icono-input");
 
 const lista = document.getElementById("lista-uno");
 
-const agregartarea = (tarea) => {
+
+const hecho = "fa-check-circle";  
+ 
+const pendiente = "fa-circle";
+
+const tachado = "tachado";
+
+
+
+
+const agregartarea = (tarea, check) => {
+    const estado = check ? hecho : pendiente;
+    const tachar = check ? tachado : "";
     const elemento = `<li class="estas-son" id="estas-son">
-                <i class="fas fa-circle co check" id="check" data-="check"></i>
-                <p class="tarea tachado" id="tarea">${tarea}</p>
-                <i class="fas fa-trash de borrar" id="borrar" data-="borrar"></i>
+                <i class="far ${estado} check" id="check" data="check"></i>
+                <p class="tarea ${tachar}" id="tarea">${tarea}</p>
+                <i class="fas fa-trash de borrar" id="borrar" data="borrar"></i>
             </li>`
     
     lista.insertAdjacentHTML("beforeend",elemento);
 };
+
+
+const tareaRealizada = (element) => {
+element.classList.toggle(hecho);
+element.classList.toggle(pendiente);
+
+element.parentNode.querySelector(".tarea").classList.toggle(tachado); 
+};
+
+
+
+const tareaEliminada = (element) => {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+};
+
+
+const cambiarEstilos = () => {
+    const link = document.getElementById("estilo");
+    link.href = link.href.includes("style.css") ? "style2.css" : "style.css";
+};
+
 
 
 //Llamar funcion
@@ -45,10 +78,23 @@ document.addEventListener("keyup", (e) => {
         input.value = "";    
     }
 }
+);
+
+
+lista.addEventListener("click", function (event) {
+  const element = event.target;
+  const elementData = element.atributes.data.value;
+
+  if (elementData == "check") {
+    tareaRealizada (element)
+}
+
+else if (elementData == "borrar") {
+    tareaEliminada(element)
+}
+
+}
 )
 
-const cambiarEstilos = () => {
-    const link = document.getElementById("estilo");
-    link.href = link.href.includes("style.css") ? "style2.css" : "style.css";
-};
+
 
